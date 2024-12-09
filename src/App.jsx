480d31a15ecec6981b3195b95dc4d836e1f2e3a3
Matheus Alves <matheuszinho26@outlook.com>
2024-12-08 23:58:34 -0300

    Mudanças sendo feitas

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+
+export function App() {
+  return (
+    <main>
+      <Header />
+
+      <Home />
+    </main>
+  );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import "./header.module.css";
+
+export function Header() {
+  return (
+    <header>
+      <h1>Meu Repositório - Projeto Unimar</h1>
+    </header>
+  );
+}
